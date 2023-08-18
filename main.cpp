5df859817a24f4b0a9ad4bb6fadd0b860c67f73a
KeyEno2k2 <igor.jurewicz.wej@gmail.com>
2023-08-18 13:53:22 +0200

    Lepsza Wersja

--- main.cpp
+++ main.cpp
@@ -1,96 +1,126 @@
-#include <iostream>
+#include<iostream>
 #include <conio.h>
+
 using namespace std;
 
-bool gameover;
-const int width = 20;
-const int height = 17;
-
-int x, y, fruitX, fruitY, score;
-int tailX[100], tailY[100]; //Koordynaty weza
-int nTail;
-
-enum eDirection {
+enum eDirection{
     STOP = 0,
     LEFT,
     RIGHT,
     UP,
     DOWN
-}; // Kontrole
+};
+eDirection dira;
+bool gameOver;
+void setup();
+void Draw();
+void Input();
+void Logic();
+void Logic2();
+const int width = 20;
+const int height = 20;
+int x,y,fruitX, fruitY, score;
+int tailx[100], taily[100];
+int ntail;
 
-eDirection dir;
+int main(){
+    char choice;
+    cout << "\n\t-------------------------------\n";
+    cout << "\t\t Snake in C++\n";
+    cout << "\t-------------------------------\n\n";
+    cout << "Please Ender Game Difficulty: \n\n";
+    cout << "PLay Hard Mode for Press : x\n\n";
+    cout << "PLay Easy Mode for Press: z\n\n";
+    cin >> choice;
+    switch(choice)
+    {
+        case 'x':
+            setup();
+            while(!gameOver)
+            {
+                Draw();
+                Input();
+                Logic2();
+                _sleep(30);
+            }
+            break;
 
-void Setup(){
-    gameover = false;
-    dir = STOP;
+        case 'z':
+            setup();
+            while(!gameOver)
+            {
+                Draw();
+                Input();
+                Logic();
+                _sleep(30);
+            }
+
+        default:
+            cout << "Invalid!! PLease sellect correct Option!";
+            break;
+    }
+
+    getch();
+    return 0;
+}
+
+void setup(){
+    gameOver = false;
+    dira = STOP;
     x = width / 2;
-    y = height /2;
-    fruitX = rand() % width; // Ustawia owoc w randomowym miejscu na mapie
-    fruitY = rand() % height;
+    y = height / 2;
+    fruitX = rand()%width;
+    fruitY = rand()%height;
+    score = 0;
 }
 
 void Draw(){
     system("cls");
-    for (int i = 0; i < width; i++){
-        cout << "☐";
-        cout << endl;
+    cout << "\n\t\t     :SNAKE GAME:\n";
+    cout << "\t\t";
+    for (int i = 0; i < width - 8; i++){
+        cout << "||";
     }
-    for (int i = 0; i < height; i++){
-        for (int j = 0; j < width; j++){
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             if (j == 0)
-                cout << "☐"; //Sciany
+            {
+                cout << "\t\t||";
+            }
             if (i == y && j == x)
-                cout << "⊗"; // Ogon weza
-            else if (i == fruitY && j == fruitY)
-                cout << "☢";
-            else {
-            bool print = false;
-            for (int k = 0; k < nTail; k++){
-                if (tailX[k] == j && tailY[k] == 1){
-                    cout << "*";
-                    print = true;
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "*";
+            else
+            {
+                bool print = false;
+                for (int k = 0; k < ntail; k++)
+                {
+                    if(tailx[k] == j && taily[k] == i)
+                    {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                {
+                    cout << " ";
                 }
             }
-            if (!print) cout << "";
-            }
             if (j == width - 1)
-                cout << "#";
+                cout << "||";
         }
         cout << endl;
     }
-    for (int i = 0; i < width + 2; i++){
-        cout << "#";
-        cout << endl;
-        cout << "Score:" << score << endl;
+    cout << "\t\t";
+    for (int i = 0; i < width - 8; i ++)
+    {
+        cout << "||";
     }
-}
-
-void Input(){
-    if(_kbhit()){
-        switch (_getch()){
-            case 'a':
-                dir = LEFT;
-                break;
-            case 'd':
-                dir = RIGHT;
-                break;
-            case 's':
-                dir = DOWN;
-                break;
-            case 'w':
-                dir = UP;
-                break;
-            case 'x':
-                gameover = true;
-                break;
-        }
-    }
-}
-
-void alghoritm(){
-
-}
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << endl;
+    cout << "\t\t\tScore: " << score << endl;
 }
